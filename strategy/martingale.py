dc2cf03a5fa42d3dc8b5309f5e97ee73659be180
arpadpall21 <a.pall21@yahoo.fr>
2025-10-21 20:50:29 +0200

    function logic

--- strategy/martingale.py
+++ strategy/martingale.py
@@ -8,8 +8,20 @@ def martingale(start_sum: int, success_chance_percent_each_cycle: int, cycles: i
     print(f"Total Cycles: {cycles}\n")
 
     current_sum: int = start_sum
+    win_cycle_count = 0
+    lose_cycle_count = 0
 
     for cycle in range(cycles):
         print(f"Cycle: {cycle + 1}")
+        if check_success(success_chance_percent_each_cycle):
+            current_sum += 1
 
-    print(f"End Sum: {current_sum}")
+            win_cycle_count += 1
+            print(f"  Win -> current sum: {current_sum}")
+        else:
+            current_sum -= 1
+
+            lose_cycle_count += 1
+            print(f"  Lose -> current sum: {current_sum}")
+
+    print(f"\nEnd Sum: {current_sum} (win cycle count={win_cycle_count}) (lose cycle count={lose_cycle_count})")
