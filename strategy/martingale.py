6f6746c81c56c27ba6e707a730ed3ebec993ef7f
arpadpall21 <a.pall21@yahoo.fr>
2025-10-21 20:41:53 +0200

    basic printing

--- strategy/martingale.py
+++ strategy/martingale.py
@@ -1,2 +1,15 @@
-def martingale(start_sum: int, success_chance_percent_each_cycle: int, cycles: int) -> int:
-    print('some report...')
+from misc.rand_success import check_success
+
+
+def martingale(start_sum: int, success_chance_percent_each_cycle: int, cycles: int) -> None:
+    print("Stragety Name: Martingale")
+    print(f"Start Sum: {start_sum}")
+    print(f"Success Chance Percent Each Turn: {success_chance_percent_each_cycle}")
+    print(f"Total Cycles: {cycles}\n")
+
+    current_sum: int = start_sum
+
+    for cycle in range(cycles):
+        print(f"Cycle: {cycle + 1}")
+
+    print(f"End Sum: {current_sum}")
