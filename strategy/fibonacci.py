14c0b1ae5af83791b5728ce72d5bdabf397c3287
arpadpall21 <a.pall21@yahoo.fr>
2025-10-27 21:08:36 +0100

    strategy fibonacci

--- strategy/fibonacci.py
+++ strategy/fibonacci.py
@@ -9,18 +9,20 @@ from typing import Literal
 
 from misc.rand_success import check_success
 from misc.update_counters import update_counters, Counters
-from misc.log import log, log_strategy_header, log_end_game
+from misc.log import log, log_strategy_header, log_cycle_status, log_end_game
 
 
 def fibonacci(start_sum: int,
               success_chance_percent_each_cycle: int,
               cycles: int,
               log_verbose: bool,
-              options: dict[str, int] = {"target_sum_start_sum_percent": 5}) -> int:
+              options: dict[str, None | int] = {"percent_target": None}) -> int:
     log_strategy_header("Fibonacci", start_sum, success_chance_percent_each_cycle, cycles, log_verbose)
 
     current_sum: int = start_sum
-    target_sum: float = current_sum + (current_sum / 100 * options["target_sum_start_sum_percent"])
+    target_sum: int = (
+        None if options["percent_target"] is None else start_sum + (start_sum / 100 * options["percent_target"])
+    )
     current_bet: int = 1
     win_counters: Counters = Counters(0, 0, 0)
     lose_counters: Counters = Counters(0, 0, 0)
@@ -31,22 +33,22 @@ def fibonacci(start_sum: int,
             update_counters("win", win_counters, lose_counters)
             current_sum += current_bet
             next_bet: int = get_next_fibonacci_val(get_next_fibonacci_val(current_bet, "previous"), "previous")
-            log(f"  Win -> current sum: {current_sum} (current bet={current_bet}) (next bet={next_bet})", log_verbose)
 
+            log_cycle_status("Wind", current_sum, current_bet, next_bet, log_verbose)
             current_bet = next_bet
         else:
             update_counters("lose", win_counters, lose_counters)
             current_sum -= current_bet
             planned_next_bet: int = get_next_fibonacci_val(current_bet, "next")
             next_bet: int = planned_next_bet if planned_next_bet < current_sum else current_sum
-            log(f"  Lose -> current sum: {current_sum} (current bet={current_bet}) (next bet={next_bet})", log_verbose)
 
+            log_cycle_status("Lose", current_sum, current_bet, next_bet, log_verbose)
             current_bet = next_bet
-            if current_sum <= 0:
-                log_end_game("You run out of cache", current_sum, win_counters, lose_counters, True)
-                return current_sum
 
-        if current_sum >= target_sum:
+        if current_sum <= 0:
+            log_end_game("You run out of cache", current_sum, win_counters, lose_counters, True)
+            return current_sum
+        if target_sum and current_sum >= target_sum:
             log_end_game("Target Reached", current_sum, win_counters, lose_counters, True)
             return current_sum
 
