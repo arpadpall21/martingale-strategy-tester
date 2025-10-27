944fc041da020bba2aebb0c7658df46ba319a4cd
arpadpall21 <a.pall21@yahoo.fr>
2025-10-27 18:34:58 +0100

    renamed

--- strategy/__init__.py
+++ strategy/__init__.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from typing import Callable, Any, Optional
 
 from strategy.martingale import martingale
-from strategy.reverse_martingale import reverse_martingale
+from strategy.anti_martingale import anti_martingale
 from strategy.random_bet import random_bet
 from strategy.fibonacci import fibonacci
 
@@ -13,13 +13,13 @@ type StragetyFn = Callable[[int, int, int, bool, Optional[dict[str, Any]]], int]
 class Strategy():
     martingale: StragetyFn
     random_bet: StragetyFn
-    reverse_martingale: StragetyFn
+    anti_martingale: StragetyFn
     fibonacci: StragetyFn
 
 
 strategy = Strategy(
     martingale=martingale,
-    reverse_martingale=reverse_martingale,
+    anti_martingale=anti_martingale,
     random_bet=random_bet,
     fibonacci=fibonacci,
 )
