3a4f0edeb59f9cf9542a8dee4a337941a7777808
arpadpall21 <a.pall21@yahoo.fr>
2025-10-29 08:05:17 +0100

    fibonacci

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # strategy to use, (available: martingale, grand_martingale, anti_martingale, fibonacci, random_bet)
-use_strategy: str = "fibonacci"
+use_strategy: str = "random_bet"
 # runs this game nr of times (with detailed reports)
 multi_game_cycles: int | None = None
 
