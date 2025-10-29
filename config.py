3dbecf566b0045e56b2cf2aa959976b3689df77e
arpadpall21 <a.pall21@yahoo.fr>
2025-10-29 07:59:20 +0100

    martingale

--- config.py
+++ config.py
@@ -9,25 +9,15 @@ base_config: dict[str, int | bool] = {
     "success_chance_percent_each_cycle": 50,
     "cycles": 10,
     "log_verbose": True,
+    "percent_target": 1,
 }
 
 # strategy specific configuration
 strategy_config: dict[str, dict[str, None | int | list[int]]] = {
-    "martingale": {
-        "percent_target": None
-    },
     "grand_martingale": {
-        "percent_target": None,
         "extra_bet": 1,
     },
-    "anti_martingale": {
-        "percent_target": None
-    },
-    "fibonacci": {
-        "percent_target": None
-    },
     "random_bet": {
-        "percent_target": None,
         "bet_range": [1, 3]
     },
 }
